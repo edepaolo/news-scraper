07228362e32356cbd55f14ff6e466c85e775a9e4
Eddie DePaolo <edepaolo@gmail.com>
2019-11-15 16:00:17 -0800

    Cleared previous attempt and working from activity

--- models/Note.js
+++ models/Note.js
@@ -1,9 +1,29 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// Note model
+// ==========
 
-const NoteSchema = new Schema({
-  body: String
+// Require mongoose
+var mongoose = require("mongoose");
+// Create the schema class using mongoose's schema method
+var Schema = mongoose.Schema;
+
+// Create the noteSchema with the schema object
+var noteSchema = new Schema({
+  // The headline is the article associate with the note
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // as is the noteText
+  noteText: String
 });
 
-const Note = mongoose.model('Note', NoteSchema);
+// Create the Note model using the noteSchema
+var Note = mongoose.model("Note", noteSchema);
+
+// Export the Note model
 module.exports = Note;
--- server.js
+++ server.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const db = require('./models');
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.use(logger('dev'));
+// Web Scraper Homework Solution Example
+// (be sure to watch the video to see
+// how to operate the site in the browser)
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+
+// Require our dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Set up our port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
+
+// Instantiate our Express App
+var app = express();
+
+// Require our routes
+var routes = require("./routes");
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.engine(
-  'handlebars',
-  exphbs({
-      defaultLayout: 'main',
-      layoutsDir: __dirname + '/views/layouts/',
-      partialsDir: __dirname + '/views/partials/'
-  })
-);
-app.set('view engine', 'handlebars');
+// Make public a static folder
+app.use(express.static("public"));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// Have every request go through our route middleware
+app.use(routes);
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
+// Listen on the port
 app.listen(PORT, function() {
-  console.log('App running on port ' + PORT + '!');
+  console.log("Listening on port: " + PORT);
 });
-
-module.exports = app;
